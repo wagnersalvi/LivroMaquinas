ca1565b8c2e33327b37f9755d23ffe0d1db287a6
Wagner Salvi <wagner@wagnersalvi.eti.br>
2025-03-24 21:54:03 -0300

    clustering

---
+++ Clustering/Models.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ML.Data;
+
+namespace Clustering;
+
+class Cliente
+{
+	[LoadColumn(0)] public float GastoMensal;
+	[LoadColumn(1)] public float FrequenciaCompras;
+}
+
+class ClusterPrediction
+{
+	[ColumnName("PredictedLabel")] public uint Cluster;
+}
---
+++ Clustering/Program.cs
@@ -0,0 +1,29 @@
+﻿using Clustering;
+using Microsoft.ML;
+
+class Program
+{
+	static void Main()
+	{
+		var context = new MLContext();
+		var dados = new[]
+		{
+			new Cliente { GastoMensal = 200, FrequenciaCompras = 5 },
+			new Cliente { GastoMensal = 50, FrequenciaCompras = 2 },
+			new Cliente { GastoMensal = 1000, FrequenciaCompras = 10 },
+			new Cliente { GastoMensal = 150, FrequenciaCompras = 3 }
+		};
+
+		var dataView = context.Data.LoadFromEnumerable(dados);
+		var pipeline = context.Transforms.Concatenate("Features", "GastoMensal", "FrequenciaCompras")
+						.Append(context.Clustering.Trainers.KMeans("Features", numberOfClusters: 3));
+
+		var modelo = pipeline.Fit(dataView);
+		var predictor = context.Model.CreatePredictionEngine<Cliente, ClusterPrediction>(modelo);
+
+		var novoCliente = new Cliente { GastoMensal = 120, FrequenciaCompras = 4 };
+		var resultado = predictor.Predict(novoCliente);
+
+		Console.WriteLine($"O cliente foi classificado no cluster: {resultado.Cluster}");
+	}
+}
